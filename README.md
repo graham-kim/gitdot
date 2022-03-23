1b1efa5617afe79f4a2e375b6c9b8aabf9fc25f2
Graham Kim <graham.kim@cantab.net>
2022-03-23 22:38:12 +0000

    Parse standard GitHub merge commit messages
    Add some information to merge commits as they are the interesting
    features of git history.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # gitdot
 Git history to DOT language translation
+
+# Installing Pre-requisites
+
+Do this in this repository's root directory:
+`python -m pip install -r requirements.txt`
+
+You will also need to have installed graphviz
+
+# Sample Usage
+
+Run this from your repository's root directory:
+`git log HEAD~8..HEAD --format="%p -> %h" | python path/to/gitdot.py | dot -Tpng > out.png`
--- gitdot.py
+++ gitdot.py
@@ -1,13 +1,60 @@
 #--format="%p -> %h"
 
 import typing as tp
+import os
 import sys
 from collections import namedtuple
+from git import Repo
+import re
+
+merge_pr_regex = re.compile(r"Merge pull request #(\d+) from \S+/(\S+)")
+merge_local_branch_regex = re.compile(r"Merge branch '(\S+)' into (\S+)")
+merge_remote_branch_regex = re.compile(r"Merge remote-tracking branch 'origin/(\S+)' into (\S+)")
+
+class MergeInfo:
+    def __init__(self):
+        self.src: str = None
+        self.dst: str = None
+        self.summary: str = None
+
+def parse_merge_message(summary: str) -> MergeInfo:
+    info = MergeInfo()
+    m = merge_pr_regex.match(summary)
+    if m:
+        info.src = f'PR #{m.group(1)} {m.group(2)}'
+        return info
+
+    m = merge_local_branch_regex.match(summary)
+    if not m:
+        m = merge_remote_branch_regex.match(summary)
+    if not m:
+        info.summary = summary
+        return info
+
+    info.src = m.group(1)
+    info.dst = m.group(2)
+    return info
 
 def describe_commit_in_dot(commit_hash: str, fillcolor: str = "yellow") -> str:
     line = f'    _{commit_hash}'
     if fillcolor:
         line += ' [fillcolor="yellow"]'
+    return line
+
+def describe_merge_commit_in_dot(repo: Repo, commit_hash: str, fillcolor: str = "yellow") -> str:
+    commit = repo.commit(commit_hash)
+    line = describe_commit_in_dot(commit_hash, fillcolor)
+    info = parse_merge_message(commit.summary)
+
+    line = line.strip(']') + f', label="{commit_hash}\\n'
+    if info.summary is not None:
+        line += info.summary
+    else:
+        line += f'src: {info.src}'
+        if info.dst is not None:
+            line += f'\\ndst: {info.dst}'
+    line += '"]'
+
     return line
 
 def translate_to_dot(parent: str, child: str, highlight_link: bool=False) -> str:
@@ -44,7 +91,7 @@ class ParseResults:
         self.multi_parent_dot_lines: tp.List[str] = []
         self.multi_parent_nodes: tp.List[str] = []
 
-def parse_stdin() -> ParseResults:
+def parse_stdin(repo: Repo) -> ParseResults:
     results = ParseResults()
     for line in sys.stdin:
         if "->" not in line:
@@ -61,7 +108,7 @@ def parse_stdin() -> ParseResults:
         parents = lhs.strip().split(' ')
         child = rhs.strip()
         if len(parents) > 1:
-            results.multi_parent_nodes.append( describe_commit_in_dot(child) )
+            results.multi_parent_nodes.append( describe_merge_commit_in_dot(repo, child) )
             for p in parents:
                 results.multi_parent_dot_lines.append( translate_to_dot(p, child) )
         else:
@@ -73,11 +120,11 @@ def parse_stdin() -> ParseResults:
     condense_OPCs(results.one_parent_commits)
     return results
 
-def print_dot():
+def print_dot(repo: Repo):
     print("digraph {")
     print("    rankdir=TD")
     print('    node [shape="box", style="filled", fillcolor="white"]\n')
-    results = parse_stdin()
+    results = parse_stdin(repo)
     for line in results.multi_parent_nodes:
         print(line)
     print("")
@@ -90,4 +137,4 @@ def print_dot():
     print("}")
 
 if __name__ == '__main__':
-    print_dot()
+    print_dot(Repo(os.getcwd()))
